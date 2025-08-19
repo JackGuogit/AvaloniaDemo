473c72d4eb3fe8d719fba8ca1a804f4c05751beb
JackGuogit <guoexchange@163.com>
2025-08-19 15:07:30 +0800

    TestStackPanel

---
+++ AvaloniaPrsimSimple/Views/ControlsCaseChild/TestExStackPanelView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaPrsimSimple.Views;
+
+public partial class TestExStackPanelView : UserControl
+{
+    public TestExStackPanelView()
+    {
+        InitializeComponent();
+    }
+}
