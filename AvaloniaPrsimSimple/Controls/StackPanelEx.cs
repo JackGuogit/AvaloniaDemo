318025d0841a70820fa9f812488986686da380a5
JackGuogit <guoexchange@163.com>
2025-08-22 19:33:40 +0800

    端口动画

--- AvaloniaPrsimSimple/Controls/StackPanelEx.cs
+++ AvaloniaPrsimSimple/Controls/StackPanelEx.cs
@@ -1,11 +1,16 @@
 ﻿using Avalonia;
+using Avalonia.Animation;
 using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Styling;
+using ImTools;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 namespace AvaloniaPrsimSimple.Controls
 {
     public class StackPanelEx : StackPanel
@@ -56,10 +61,43 @@ namespace AvaloniaPrsimSimple.Controls
                 double y1 = 0;
                 double y2 = finalSize.Height - secondChildSize.Height;
 
-                firstChild.Arrange(new Rect((finalSize.Width - firstChildSize.Width) / 2, y1, firstChildSize.Width, firstChildSize.Height));
+
                 secondChild.Arrange(new Rect((finalSize.Width - secondChildSize.Width) / 2, y2, secondChildSize.Width, secondChildSize.Height));
 
 
+                if (firstChild.RenderTransform is not TranslateTransform transform)
+                {
+                    transform = new TranslateTransform();
+                    firstChild.RenderTransform = transform;
+                }
+
+                var animation = new Animation
+                {
+                    Duration = TimeSpan.FromSeconds(0.1),
+                    Children =
+                    {
+                        new KeyFrame
+                        {
+                            Setters =
+                            {
+
+                                new Setter(TranslateTransform.YProperty, firstChild.Bounds.Y)
+                            },
+                            Cue = new Cue(0)
+                        },
+
+                        new KeyFrame
+                        {
+                            Setters =
+                            {
+                                new Setter(TranslateTransform.YProperty, y1)
+                            },
+                            Cue = new Cue(1)
+                        }
+                    }
+                };
+                firstChild.Arrange(new Rect((finalSize.Width - firstChildSize.Width) / 2, y1, firstChildSize.Width, firstChildSize.Height));
+                animation.RunAsync(firstChild);
 
 
             }
@@ -86,13 +124,56 @@ namespace AvaloniaPrsimSimple.Controls
 
 
 
-                for (int i = 0; i < childCount; i++)
+                for (int i = 1; i < childCount-1; i++)
                 {
                     var child = children[i];
                     var childSize = child.DesiredSize;
-                    child.Arrange(new Rect((finalSize.Width - childSize.Width) / 2, currentY, childSize.Width, childSize.Height));
+
                     currentY += childSize.Height+ gap;
+
+
+
+                    if (child.RenderTransform is not TranslateTransform transform)
+                    {
+                        transform = new TranslateTransform();
+                        child.RenderTransform = transform;
+                    }
+
+                    var animation = new Animation
+                    {
+                        Duration = TimeSpan.FromSeconds(0.8),
+                        Children =
+                        {
+                            new KeyFrame
+                            {
+                                Setters =
+                                {
+
+                                                                       new Setter(TranslateTransform.YProperty, currentY)
+                                },
+                                Cue = new Cue(0)
+                            },
+
+                            new KeyFrame
+                            {
+                                Setters =
+                                {
+ 
+                                                                        new Setter(TranslateTransform.YProperty, currentY-gap-childSize.Height)
+                                },
+                                Cue = new Cue(1)
+                            }
+                        }
+                    };
+                    child.Arrange(new Rect((finalSize.Width - childSize.Width) / 2, currentY, childSize.Width, childSize.Height));
+                    animation.RunAsync(child);
                 }
+
+                var childLast = children.Last();
+                var childLastSize = childLast.DesiredSize;
+                currentY += childLastSize.Height + gap;
+                childLast.Arrange(new Rect((finalSize.Width - childLastSize.Width) / 2, currentY, childLastSize.Width, childLastSize.Height));
+
             }
 
             else
