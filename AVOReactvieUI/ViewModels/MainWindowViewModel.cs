cf54a6f090fd041a7b74cb1a752133a5bdebff7c
JackGuogit <guoexchange@163.com>
2025-09-16 11:27:21 +0800

    Interaction案例

--- AVOReactvieUI/ViewModels/MainWindowViewModel.cs
+++ AVOReactvieUI/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using ReactiveUI;
+using System.Reactive.Linq;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AVOReactiveUI.ViewModels;
 
@@ -8,9 +11,43 @@ public class MainWindowViewModel : ViewModelBase
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
+
+
+    private readonly Interaction<string, bool> _showMessageInteraction=new Interaction<string, bool>();
+
+
+    public ICommand ShowMainViewCommand => ReactiveCommand.CreateFromTask( async () =>
+    {
+        await ShowMessage("hello word");
+    });
     public MainWindowViewModel(MainViewModel mainViewModel)
     {
         CurrentViewModel= mainViewModel;
+
+
+
+
+
+
     }
     public MainViewModel CurrentViewModel { get; set; } 
+
+
+    public Interaction<string, bool> ShowMessageInteraction => _showMessageInteraction;
+
+
+    public async Task ShowMessage(string message)
+    {
+        var result = await _showMessageInteraction.Handle(message);
+        // 处理结果
+
+        if (result != null) 
+        {
+            // 处理结果
+        }
+    }
+
+
+
+
 }
--- AVOReactvieUI/Views/MainWindow.axaml.cs
+++ AVOReactvieUI/Views/MainWindow.axaml.cs
@@ -2,6 +2,8 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.ReactiveUI;
 using AVOReactiveUI.ViewModels;
+using ReactiveUI;
+using System.Reactive.Disposables;
 
 namespace AVOReactiveUI.Views;
 
@@ -10,6 +12,19 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
     public MainWindow()
     {
         InitializeComponent();
+        this.WhenActivated(disposables =>
+        {
+            // 处理 ShowMessageInteraction 交互
+            ViewModel?.ShowMessageInteraction.RegisterHandler(async interaction =>
+            {
+                var message = interaction.Input;
+                // 显示消息对话框
+                var result = true;
+                // 返回结果给 ViewModel
+                interaction.SetOutput(result);
+            }).DisposeWith(disposables);
+        });
+
     }
 
 
