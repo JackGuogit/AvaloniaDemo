b8077d6c8f07f89e730f258caff0d7f82dfe1af4
JackGuogit <guoexchange@163.com>
2025-09-17 18:11:34 +0800

    Flush

--- AvaloniaCross/AvaloniaCross/ViewModels/MainViewModel.cs
+++ AvaloniaCross/AvaloniaCross/ViewModels/MainViewModel.cs
@@ -1,25 +1,31 @@
-﻿
-using Akavache;
+﻿using Akavache;
 using Splat;
 using System;
 using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 
 namespace AvaloniaCross.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
         public string Greeting { get; } = "Welcome to Avalonia!";
-        public MainViewModel() 
+
+        public MainViewModel()
         {
             IAkavacheInstance? akavacheInstance = Splat.Locator.Current.GetService<IAkavacheInstance>();
-            
+
             IBlobCache? localMachine = akavacheInstance.LocalMachine;
-            localMachine.InsertObject("sss", new {sss="sss",
-                                                    bbb="aaa"});
+            localMachine.InsertObject("sss", new
+            {
+                sss = "sss",
+                bbb = "aaa"
+            });
+            //localMachine.Vacuum();
+            IObservable<System.Reactive.Unit> observable1 = localMachine.Flush();
+
 
 
             IObservable<dynamic?> observable = localMachine.GetObject<dynamic>("sss");
-
         }
     }
 }
