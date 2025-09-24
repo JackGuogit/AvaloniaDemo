adc1a8460e9597a9605eddb5a7488f82319cc7b6
JackGuogit <guoexchange@163.com>
2025-09-24 17:28:38 +0800

    UrsaDemo

---
+++ UrsaDemo/Views/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Ursa.ReactiveUIExtension;
+
+namespace UrsaDemo.Views
+{
+    public partial class MainWindow : ReactiveUrsaWindow<Window>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
