4744fe7dba9dbf37cb9209757c35116056bd0706
JackGuogit <guoexchange@163.com>
2025-09-24 16:35:10 +0800

    WrapPanel

--- AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,26 @@
-﻿namespace AvaloniaApplication1.ViewModels;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace AvaloniaApplication1.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
+
+    public ICommand ClickCommand { get; }
+
+    public ObservableCollection<string> Items { get; } = new();
+
+    public MainWindowViewModel()
+    {
+        ClickCommand = new RelayCommand(OnClick);
+    }
+
+    private void OnClick()
+    {
+    }
 }
