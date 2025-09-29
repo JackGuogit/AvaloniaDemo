c73a92e674122e017034f912d748153f7f8a534a
JackGuogit <guoexchange@163.com>
2025-09-29 10:12:11 +0800

    Breadcrumb

--- UrsaDemo/ViewModels/BreadcrumbViewModel.cs
+++ UrsaDemo/ViewModels/BreadcrumbViewModel.cs
@@ -21,43 +21,25 @@ namespace UrsaDemo.ViewModels
             set => this.RaiseAndSetIfChanged(ref _breadcrumbItems, value);
         }
 
-        private string _text;
-
-        public string Text
-        {
-            get => _text;
-            set => this.RaiseAndSetIfChanged(ref _text, value);
-        }
-
-        private string _icon;
-
-        public string Icon
-        {
-            get => _icon;
-            set => this.RaiseAndSetIfChanged(ref _icon, value);
-        }
-
-        private bool _isActive;
-
-        public bool IsActive
-        {
-            get => _isActive;
-            set => this.RaiseAndSetIfChanged(ref _isActive, value);
-        }
+        public ICommand ActiveCommand { get; set; }
 
         public BreadcrumbViewModel()
         {
-            BreadcrumbItems.Add(new BreadcrumbItem("Home", "M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z", false));
-            BreadcrumbItems.Add(new BreadcrumbItem("Category", "M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zm0-8h14V7H7v2z", false));
-            BreadcrumbItems.Add(new BreadcrumbItem("Subcategory", "M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z", true));
+            ActiveCommand = ReactiveCommand.Create<object>((o) =>
+            { 
+                IEnumerable<BreadcrumbItem> enumerable = BreadcrumbItems.Where(b => b.Equals(o));
+                
+                // Handle breadcrumb item click
+                Debug.WriteLine($"Breadcrumb '{o}' clicked.");
+            });
+            BreadcrumbItems.Add(new BreadcrumbItem("Home", "M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z", ActiveCommand, false));
+            BreadcrumbItems.Add(new BreadcrumbItem("Category", "M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zm0-8h14V7H7v2z", ActiveCommand, false));
+            BreadcrumbItems.Add(new BreadcrumbItem("Subcategory", "M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z", ActiveCommand, true));
         }
 
         private void GotoCommand()
         {
-
         }
-
-
     }
 
     public class BreadcrumbItem : ViewModelBase
@@ -77,16 +59,12 @@ namespace UrsaDemo.ViewModels
             }
         }
 
-        public BreadcrumbItem(string text, string icon, bool isActive = false)
+        public BreadcrumbItem(string text, string icon, ICommand command, bool isActive = false)
         {
             Text = text;
             Icon = icon;
             IsActive = isActive;
-            ActiveCommand = ReactiveCommand.Create<int>((o) =>
-            {
-                // Handle breadcrumb item click
-                Debug.WriteLine($"Breadcrumb '{o}' clicked.");
-            });
+            ActiveCommand = command;
         }
     }
 }
