010d98c6bab344951fe8145f24befffba0bdbabb
JackGuogit <guoexchange@163.com>
2025-10-28 15:09:20 +0800

    test DynamicData SourceList

--- FirstDemo/ViewModels/MainViewModel.cs
+++ FirstDemo/ViewModels/MainViewModel.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 
 namespace FirstDemo.ViewModels
 {
-    public class MainViewModel:ViewModelBase
+    public class MainViewModel : ViewModelBase
     {
-
         public string Title { get; set; } = "sssssssss";
-
     }
 }
