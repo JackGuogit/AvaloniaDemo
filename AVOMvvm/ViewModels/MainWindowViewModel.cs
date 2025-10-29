7f0ddd7962e38c2c55925b6dbdee8390eee233a8
JackGuogit <guoexchange@163.com>
2025-10-29 16:51:06 +0800

    Benchmark

--- AVOMvvm/ViewModels/MainWindowViewModel.cs
+++ AVOMvvm/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,35 @@
-﻿namespace AVOMvvm.ViewModels;
+﻿using BenchmarkDotNet.Attributes;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Windows.Input;
+
+namespace AVOMvvm.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
+
+    private string greeting = "Welcome to Avalonia!";
+
+    public string Greeting
+    {
+        get { return greeting; }
+        set { this.SetProperty(ref greeting, value); }
+    }
+
+    public ICommand GreetingCommand { get; set; }
+
+    public MainWindowViewModel()
+    {
+        GreetingCommand = new RelayCommand(GreetingM);
+    }
+
+    private int i = 1;
+
+    private void GreetingM()
+    {
+        Greeting = Greeting + i++;
+    }
+
 #pragma warning restore CA1822 // Mark members as static
 }
